f828281d86bf4fef479f3fc9f4e80f660a329e7e
Spotpuff <spotpuff@gmail.com>
2022-08-23 23:24:08 -0400

    update: new script RenameSubtitlesController.ps1 added

---
+++ RenameSubtitlesController.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $Path = "M:\downloads"
+)
+
+$directories = Get-ChildItem -Path $Path -Directory
+if ($directories.count -gt 0)
+{
+    ForEach-Object { & $PSScriptRoot\Rename-Subtitles.ps1 -Path $_.FullName }
+}
+else {
+    Write-Output "No media directories found in $Path."
+}
