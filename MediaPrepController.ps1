9d5f4cb81cfad66859d7f003619876f7d7ef342a
spotpuff <32026605+spotpuff@users.noreply.github.com>
2022-11-05 03:36:03 -0400

    Feature/chanp remove samples (#2)
    * feature: modified extra file removal to remove sample subdirectories
    
    * update: updated media cleanup controller to handle TV shows or movies

--- MediaPrepController.ps1
+++ MediaPrepController.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param (
-# Parameter help description
-    [Parameter(Mandatory=$false)]
+    # Parameter help description
+    [Parameter(Mandatory = $false)]
     [string]
     $Path = 'M:\downloads'
 )
@@ -10,9 +10,21 @@ $directories = Get-ChildItem -Path $Path -Directory
 if ($directories.count -gt 0)
 {
     $directories | ForEach-Object {
-            & $PSScriptRoot\Rename-Subtitles.ps1 -Path $_.FullName
-            & $PSScriptRoot\Remove-ExtraFiles.ps1 -Path $_.FullName
+        # remove extraneous files
+        & $PSScriptRoot\Remove-ExtraFiles.ps1 -Path $_.FullName    
+
+        # check if there are multiple files over 5MB (anything less is assumed to be a subtitle)
+        # if there are, assume it's a TV series
+        $videoFiles = Get-ChildItem $_.FullName | Where-Object { $_.length -gt '5MB' }
+        if ($videoFiles.count -gt 1)
+        {
+            & $PSScriptRoot\Rename-SubtitlesTv.ps1 -Path $_.FullName
         }
+        else
+        {
+            & $PSScriptRoot\Remove-Subtitles.ps1 -Path $_.FullName
+        }
+    }
 }
 else
 {
