992628bfa4dabaf10ce026b40ad8145c14854096
Spotpuff <spotpuff@gmail.com>
2023-10-01 05:13:17 -0400

    Added better console output.

--- MediaPrepController.ps1
+++ MediaPrepController.ps1
@@ -11,6 +11,7 @@ if ($directories.count -gt 0)
 {
     $directories | ForEach-Object {
         # remove extraneous files
+        Write-Output "Removing extra files in: $($_.fullname)"
         & $PSScriptRoot\Remove-ExtraFiles.ps1 -Path $_.FullName
 
         # check if there are multiple files over 5MB (anything less is assumed to be a subtitle)
@@ -18,10 +19,12 @@ if ($directories.count -gt 0)
         $videoFiles = Get-ChildItem -LiteralPath $_.FullName | Where-Object { $_.length -gt '5MB' }
         if ($videoFiles.count -gt 1)
         {
+            Write-Output "Renaming tv subtitles in: $($_.fullname)"
             & $PSScriptRoot\Rename-SubtitlesTv.ps1 -Path $_.FullName
         }
         else
         {
+            Write-Output "Renaming movie subtitles in: $($_.fullname)"
             & $PSScriptRoot\Rename-Subtitles.ps1 -Path $_.FullName
         }
     }
