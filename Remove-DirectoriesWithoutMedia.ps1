c9995c6902d4d4f661a60f22c9279002a5ecb241
Philip Chan <spotpuff@gmail.com>
2024-03-12 22:01:45 -0400

    fixed issue with Split-Path using -LiteralPath parameter causing an error.

--- Remove-DirectoriesWithoutMedia.ps1
+++ Remove-DirectoriesWithoutMedia.ps1
@@ -23,7 +23,7 @@ Param
 $mediaDirectories = Get-ChildItem -LiteralPath $Path -Directory
 $mediaDirectories | ForEach-Object {
     $mediaFiles = Get-ChildItem -File -Recurse | Where-Object { $_.Extension -in $MediaFileTypes }
-    $baseDirectory = Split-Path -LiteralPath $_.FullName -Leaf
+    $baseDirectory = Split-Path -Path $_.FullName -Leaf
     if ($mediaFiles.count -eq 0)
     {
         Write-Host "$($baseDirectory) was empty; removing it."
